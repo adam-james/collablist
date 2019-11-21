8493897631a0fa6b07d53641a072035c3c6ccbf1
Adam Sulewski <adam.j.sulewski@gmail.com>
2019-11-21 08:14:49 -0500

    create playback server

---
+++ lib/spoti/playback/playback_server.ex
@@ -0,0 +1,58 @@
+defmodule Spoti.Playback.PlaybackServer do
+  use GenServer
+
+  alias Spoti.Playback.PlaybackServer
+
+  defstruct playlist_id: -1,
+            current_track: %Spotify.Track{},
+            progress_ms: 0,
+            is_playing: false,
+            timer_ref: nil
+
+  # Client
+
+  def start_link(playlist_id) do
+    GenServer.start_link(__MODULE__, playlist_id)
+  end
+
+  def get_state(pid) do
+    GenServer.call(pid, :get_state)
+  end
+
+  def play(pid) do
+    GenServer.call(pid, :play)
+  end
+
+  def pause(pid) do
+    GenServer.call(pid, :pause)
+  end
+
+  # Server
+
+  def init(playlist_id) do
+    state = %PlaybackServer{playlist_id: playlist_id}
+    {:ok, state}
+  end
+
+  def handle_call(:get_state, _from, state) do
+    {:reply, state, state}
+  end
+
+  def handle_call(:play, _from, state) do
+    timer_ref = Process.send_after(self(), :tick, 1000)
+    next_state = %{state | is_playing: true, timer_ref: timer_ref}
+    {:reply, next_state, next_state}
+  end
+
+  def handle_call(:pause, _from, state) do
+    Process.cancel_timer(state.timer_ref)
+    next_state = %{state | is_playing: false, timer_ref: nil}
+    {:reply, next_state, next_state}
+  end
+
+  def handle_info(:tick, state) do
+    timer_ref = Process.send_after(self(), :tick, 1000)
+    next_state = %{state | progress_ms: state.progress_ms + 1000, timer_ref: timer_ref}
+    {:noreply, next_state}
+  end
+end
---
+++ test/spoti/playback/playback_server_test.exs
@@ -0,0 +1,90 @@
+defmodule Spoti.Playback.PlaybackServerTest do
+  use ExUnit.Case, async: true
+
+  alias Spoti.Playback.PlaybackServer
+
+  describe "start_link/1" do
+    test "returns pid" do
+      playlist_id = 123
+      {:ok, pid} = PlaybackServer.start_link(playlist_id)
+      assert is_pid(pid)
+    end
+  end
+
+  describe "get_state/1" do
+    test "returns state" do
+      playlist_id = 123
+      {:ok, pid} = PlaybackServer.start_link(playlist_id)
+      state = PlaybackServer.get_state(pid)
+      assert state == %PlaybackServer{playlist_id: playlist_id}
+    end
+  end
+
+  describe "play/1" do
+    setup do
+      playlist_id = 123
+      {:ok, pid} = PlaybackServer.start_link(playlist_id)
+      state = PlaybackServer.get_state(pid)
+      {:ok, pid: pid, state: state}
+    end
+
+    test "sets is_playing to true", %{pid: pid} do
+      next_state = PlaybackServer.play(pid)
+      assert next_state.is_playing == true
+      assert PlaybackServer.get_state(pid).is_playing == true
+    end
+
+    test "sets time_ref", %{pid: pid} do
+      next_state = PlaybackServer.play(pid)
+      assert is_reference(next_state.timer_ref)
+      assert is_reference(PlaybackServer.get_state(pid).timer_ref)
+    end
+
+    test "starts progress", %{pid: pid} do
+      # TODO can you mock time instead of sleeping?
+      PlaybackServer.play(pid)
+      :timer.sleep(1000)
+      state = PlaybackServer.get_state(pid)
+      assert state.progress_ms == 1000
+      :timer.sleep(1000)
+      state2 = PlaybackServer.get_state(pid)
+      assert state2.progress_ms == 2000
+    end
+  end
+
+  describe "pause/1" do
+    setup do
+      playlist_id = 123
+      {:ok, pid} = PlaybackServer.start_link(playlist_id)
+      state = PlaybackServer.get_state(pid)
+      {:ok, pid: pid, state: state}
+    end
+
+    test "sets is_playing to false", %{pid: pid} do
+      next_state = PlaybackServer.play(pid)
+      assert next_state.is_playing == true
+      next_state2 = PlaybackServer.pause(pid)
+      assert next_state2.is_playing == false
+      assert PlaybackServer.get_state(pid).is_playing == false
+    end
+
+    test "removes time_ref", %{pid: pid} do
+      next_state = PlaybackServer.play(pid)
+      assert is_reference(next_state.timer_ref)
+      next_state2 = PlaybackServer.pause(pid)
+      assert next_state2.timer_ref == nil
+      assert PlaybackServer.get_state(pid).timer_ref == nil
+    end
+
+    test "stops progress", %{pid: pid} do
+      PlaybackServer.play(pid)
+      :timer.sleep(1000)
+      state = PlaybackServer.get_state(pid)
+      assert state.progress_ms == 1000
+      PlaybackServer.pause(pid)
+      :timer.sleep(1000)
+      state2 = PlaybackServer.get_state(pid)
+      assert state2.progress_ms == 1000
+    end
+  end
+end
