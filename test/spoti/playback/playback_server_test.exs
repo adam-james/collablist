887c58c335bb747c4e448d8732facb8b2386eabd
Adam Sulewski <adam.j.sulewski@gmail.com>
2019-11-28 10:27:27 -0500

    test playback server

--- test/spoti/playback/playback_server_test.exs
+++ test/spoti/playback/playback_server_test.exs
@@ -1,95 +1,82 @@
 defmodule Spoti.Playback.PlaybackServerTest do
-  use ExUnit.Case, async: true
+  use Spoti.DataCase
 
   # NOTE to exclude tests that sleep and thus take more time,
   # run `mix test --exclude sleeps`.
 
-  alias Spoti.Playback.PlaybackServer
+  import Mock
 
-  describe "start_link/1" do
-    test "returns pid" do
-      playlist_id = 123
-      {:ok, pid} = PlaybackServer.start_link(playlist_id)
-      assert is_pid(pid)
-    end
+  alias Spoti.Auth
+  alias Spoti.Playback.PlaybackServer
+  alias Spoti.Playlists
+  alias Spoti.Profiles
+
+  setup_with_mocks [
+    {Spotify.Track, [], [get_tracks: fn _, _ -> {:ok, [%Spotify.Track{}]} end]},
+    {Spotify.Player, [], [play: fn _, _ -> :ok end, pause: fn _ -> :ok end]}
+  ] do
+    {:ok, profile} = Profiles.find_or_create_profile(%{id: "123", display_name: "Tester"})
+
+    {:ok, _} =
+      Auth.create_credential(%{profile_id: profile.id, access_token: "123", refresh_token: "456"})
+
+    {:ok, playlist} = Playlists.create_playlist(%{profile_id: profile.id, name: "Test Playlist"})
+
+    {:ok, _track} =
+      Playlists.create_track(%{
+        spotify_id: "789",
+        added_by_id: profile.id,
+        playlist_id: playlist.id
+      })
+
+    {:ok, pid} = PlaybackServer.start_link(playlist.id)
+
+    {:ok, playlist: playlist, profile: profile, pid: pid}
   end
 
   describe "get_state/1" do
-    test "returns state" do
-      playlist_id = 123
-      {:ok, pid} = PlaybackServer.start_link(playlist_id)
+    test "returns state", %{playlist: playlist, pid: pid} do
       state = PlaybackServer.get_state(pid)
-      assert state == %PlaybackServer{playlist_id: playlist_id}
+      assert state == %PlaybackServer{playlist_id: playlist.id, current_track: %Spotify.Track{}}
     end
   end
 
   describe "play/1" do
-    setup do
-      playlist_id = 123
-      {:ok, pid} = PlaybackServer.start_link(playlist_id)
-      state = PlaybackServer.get_state(pid)
-      {:ok, pid: pid, state: state}
+    test "it does not play when no current track", %{pid: pid} do
+      :sys.replace_state(pid, fn state -> %{state | current_track: nil} end)
+      next_state = PlaybackServer.play(pid)
+      assert next_state.current_track == nil
+      assert next_state.is_playing == false
     end
 
-    test "sets is_playing to true", %{pid: pid} do
+    test "it plays when current track", %{pid: pid} do
       next_state = PlaybackServer.play(pid)
+      assert next_state.current_track == %Spotify.Track{}
       assert next_state.is_playing == true
-      assert PlaybackServer.get_state(pid).is_playing == true
-    end
-
-    test "sets time_ref", %{pid: pid} do
-      next_state = PlaybackServer.play(pid)
       assert is_reference(next_state.timer_ref)
-      assert is_reference(PlaybackServer.get_state(pid).timer_ref)
-    end
-
-    @tag :sleeps
-    test "starts progress", %{pid: pid} do
-      # TODO can you mock time instead of sleeping?
-      PlaybackServer.play(pid)
-      :timer.sleep(1000)
-      state = PlaybackServer.get_state(pid)
-      assert state.progress_ms == 1000
-      :timer.sleep(1000)
-      state2 = PlaybackServer.get_state(pid)
-      assert state2.progress_ms == 2000
+      assert_called(Spotify.Player.play(:_, :_))
     end
   end
 
   describe "pause/1" do
-    setup do
-      playlist_id = 123
-      {:ok, pid} = PlaybackServer.start_link(playlist_id)
-      state = PlaybackServer.get_state(pid)
-      {:ok, pid: pid, state: state}
-    end
-
-    test "sets is_playing to false", %{pid: pid} do
+    test "pauses playback", %{pid: pid} do
       next_state = PlaybackServer.play(pid)
       assert next_state.is_playing == true
-      next_state2 = PlaybackServer.pause(pid)
+      {:ok, next_state2} = PlaybackServer.pause(pid)
       assert next_state2.is_playing == false
-      assert PlaybackServer.get_state(pid).is_playing == false
-    end
-
-    test "removes time_ref", %{pid: pid} do
-      next_state = PlaybackServer.play(pid)
-      assert is_reference(next_state.timer_ref)
-      next_state2 = PlaybackServer.pause(pid)
       assert next_state2.timer_ref == nil
-      assert PlaybackServer.get_state(pid).timer_ref == nil
     end
 
-    @tag :sleeps
-    test "stops progress", %{pid: pid} do
-      PlaybackServer.play(pid)
-      :timer.sleep(1000)
-      state = PlaybackServer.get_state(pid)
-      assert state.progress_ms == 1000
-      PlaybackServer.pause(pid)
-      :timer.sleep(1000)
-      state2 = PlaybackServer.get_state(pid)
-      assert state2.progress_ms == 1000
+    test "returns Spotify error", %{pid: pid} do
+      with_mock Spotify.Player,
+        play: fn _, _ -> :ok end,
+        pause: fn _ -> {:error, "Could not pause."} end do
+        next_state = PlaybackServer.play(pid)
+        {:error, "Could not pause."} = PlaybackServer.pause(pid)
+        next_state2 = PlaybackServer.get_state(pid)
+        assert next_state2.is_playing == true
+        assert is_reference(next_state.timer_ref)
+      end
     end
   end
 end
